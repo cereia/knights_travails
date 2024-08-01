6107727ab1d61883d38afac9758da21fe30a69b5
cereia <104402370+cereia@users.noreply.github.com>
2024-08-01 16:18:29 -0400

    Add Node class with coordinate information

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,15 @@
 # frozen_string_literal: true
+
+# Node class that represents each square on the board and their coordinates
+class Node
+  attr_reader :x_coord, :y_coord
+
+  def initialize(x_coord, y_coord)
+    @x_pos = x_coord
+    @y_pos = y_coord
+  end
+
+  def to_s
+    [@x_pos, @y_pos]
+  end
+end
