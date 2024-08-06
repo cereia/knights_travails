a0700c5df9cd88a07c7db10a18aca72244d5505c
cereia <104402370+cereia@users.noreply.github.com>
2024-08-05 20:54:48 -0400

    Add knight moves, make path, and traverse children
    Create knight moves and helper methods to determine the path

--- lib/board.rb
+++ lib/board.rb
@@ -9,6 +9,19 @@ class Board
     @board = create_nodes
   end
 
+  def knight_moves(start_pos, end_pos)
+    start_node = find_node(start_pos)
+    end_node = find_node(end_pos)
+
+    queue = [start_node]
+    path = make_path(queue, end_node, [start_pos])
+    puts "You made it from #{start_pos} to #{end_pos} in #{path.length - 1} #{path.length - 1 > 1 ? 'moves' : 'move'}!"
+    puts "Here's your path:"
+    path.reverse.each { |position| p position }
+  end
+
+  private
+
   def create_nodes
     array = []
     [*0..7].each { |x_coord| [*0..7].each { |y_coord| array << Node.new([x_coord, y_coord]) } }
@@ -17,6 +30,30 @@ class Board
 
   def find_node(position)
     @board.select { |node| return node if node.node_coordinates == position }
+  end
+
+  def make_path(queue, end_node, history, visited = [])
+    return visited if visited.include?(end_node.node_coordinates)
+
+    node = queue.shift
+    if node == end_node
+      visited << node.node_coordinates
+    else
+      traverse_the_children(node, history, queue)
+      make_path(queue, end_node, history, visited)
+    end
+  end
+
+  def traverse_the_children(node, history, queue)
+    node.knight_positions.each do |position|
+      child = find_node(position)
+      next if history.include?(position)
+
+      # give each child the proper parent variable to help fill in the path later
+      child.parent = node.node_coordinates
+      queue << child
+      history << position
+    end
   end
 
   def to_s
